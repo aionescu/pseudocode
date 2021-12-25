b18b9187ebf77ceebbbb5b8f4d7ff5500f056baf
Alex Ionescu <alxi.2001@gmail.com>
2021-12-25 04:02:14 +0200

    Remove unused `Utils` functions

--- src/Utils.fs
+++ src/Utils.fs
@@ -1,5 +1,7 @@
 module Utils
 
+// Function
+
 let flip f a b = f b a
 let const' a _ = a
 
@@ -11,19 +13,25 @@ let curry4 f a b c d = f (a, b, c, d)
 let curry5 f a b c d e = f (a, b, c, d, e)
 
 let (>..) f g a b = g <| f a b
-let (..<) f g = g >.. f
 
-let cons x xs = x :: xs
+// Pair
+
+let pair a b = (a, b)
 let swap (a, b) = (b, a)
 
 let first f (a, b) = (f a, b)
 let second f (a, b) = (a, f b)
-let pair a b = (a, b)
+
+// List
+
+let cons x xs = x :: xs
 
 let rec foldr1 f = function
   | [] -> failwith "foldr1: Empty list"
   | [x] -> x
   | x :: xs -> f x (foldr1 f xs)
+
+// Result
 
 let explain e = function
   | None -> Error e
